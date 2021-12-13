1f7e9ea66c9f2c9158fd0024261a2edcdd074bc7
Nhatcao <mitomdasem@gmail.com>
2021-12-14 03:41:22 +0700

    add sample test

---
+++ test/reward-pool/reward-pool.ts
@@ -0,0 +1,3 @@
+//Có 2 functions:
+//requestWithdraw để user tạo request => object được tạo, event được emit. Khó khăn: check event ntn, fast foward thời gian ntn
+//distributeReward để admin gọi và gửi tiền cho người request => update object, tính fee chuẩn
